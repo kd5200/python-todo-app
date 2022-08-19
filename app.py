c6802ef81848e979ffa41ad511641188ab84443b
kd5200 <kareemdavis2017@fau.edu>
2022-08-19 01:39:20 -0400

    setup get and post to my db

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, url_for
+
+from asyncio import tasks
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 app = Flask(__name__)
@@ -17,7 +19,23 @@ class Todo(db.Model):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try: 
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'there was an issue adding your task'
+
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_create).all()
+        return render_template('index.html', tasks=tasks)
+    
+   
 
 if __name__ == "__main__":
     app.run(debug =True) 
