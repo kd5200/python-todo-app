e80deefcf2ce303ac4f0cd23233b600aec5193df
kd5200 <kareemdavis2017@fau.edu>
2022-08-19 00:27:46 -0400

    setup templates and added datetime and sqlalchemy to my app

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-from flask import Flask
+from distutils.log import debug
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug =True) 
