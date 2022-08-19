3af4b744a63123f5d526b66a707313fab22d437b
kd5200 <kareemdavis2017@fau.edu>
2022-08-19 00:28:12 -0400

    .

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from distutils.log import debug
+from email.policy import default
 from flask import Flask, render_template, url_for
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.string(200), nullable=False)
+    date_create = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %>' % self.id
+
 
 @app.route('/')
 def index():
